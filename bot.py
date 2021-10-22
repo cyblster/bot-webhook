b02dcb6bcf75aa4f95f6c046abc47a78e13184f8
kiusp <kiuspensky@gmail.com>
2021-10-22 19:54:16 +0500

    Add files

--- bot.py
+++ bot.py
@@ -2,35 +2,12 @@ import os
 import telebot
 from flask import Flask, request
 
-TOKEN = '1475784127:AAGNl9EXZON9wU72NY5FwfTFhTPteIAFKCg'
-APP_URL = f'https://cyblster-webhook.herokuapp.com//{TOKEN}'
-bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
-
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo(message):
-    bot.reply_to(message, message.text)
-
-
-@server.route('/' + TOKEN, methods=['POST'])
-def get_message():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return '!', 200
 
 
 @server.route('/')
 def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
-    return '!', 200
+    return request.args, 200
 
 
 if __name__ == '__main__':
