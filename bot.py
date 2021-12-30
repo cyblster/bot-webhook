8f4efae9710f5a3f412742d0206d1e2cadb3b439
kiusp <kiuspensky@gmail.com>
2021-12-31 01:26:14 +0500

    test

--- bot.py
+++ bot.py
@@ -219,7 +219,8 @@ def username_by_email():
             cursor.execute(
                 f"SELECT `telegram_username` FROM `users` WHERE LOWER(`email`) = '{email}'"
             )
-            return cursor.fetchone()[0], 200
+            fetch = cursor.fetchone()[0]
+            return fetch, 200 if fetch else "", 400
 
 
 if __name__ == '__main__':
