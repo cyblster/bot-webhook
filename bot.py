a7cb9bcde75d4d3050f3eae93ed5f076242b2277
fadeevdan77 <97974484+loginovanastya67@users.noreply.github.com>
2022-03-10 02:08:30 +0500

    Fix

--- bot.py
+++ bot.py
@@ -116,6 +116,15 @@ def message_email(message):
             )
 
 
+@server.route(f"/{APP_TOKEN}", methods=["POST"])
+def webhook():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+
+    return "!", 200
+
+
 @server.route("/add", methods=["GET"])
 def mysql_add():
     payment_link = request.args.get("payment_link")
