5e72c78538e50ee60346dd6b029b3d5bf23b01ad
fadeevdan77 <97974484+loginovanastya67@users.noreply.github.com>
2022-03-10 01:15:57 +0500

    Fix

--- bot.py
+++ bot.py
@@ -31,200 +31,40 @@ payment_rates = {
 }
 
 
-@bot.message_handler(commands=["start"], chat_types=["private"])
-def command_start(message):
-    text = "Для получения ссылки на закрытый канал Азама Ходжаева " \
-           "напишите адрес электронной почты, указанный при Вашей покупке."
-
-    bot.send_message(
-        chat_id=message.from_user.id,
-        text=text
-    )
-
-
-@bot.message_handler(regexp=email_regexp, chat_types=["private"])
-def message_email(message):
-    email = message.text.lower()
-
+def mysql_query(query):
     connection = pymysql.connect(
-        host=MYSQL_HOST,
-        user=MYSQL_USER,
-        password=MYSQL_PASSWORD,
-        database=MYSQL_DATABASE,
+        host=MYSQL_HOST, user=MYSQL_USER, passwd=MYSQL_PASSWORD,
+        db=MYSQL_DATABASE, autocommit=True
     )
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(
-                f"SELECT * FROM `users` WHERE LOWER(`email`) = '{email}'"
-            )
-            fetch = cursor.fetchone()
-
-            if fetch is None:
-                text = "Адрес электронной почты не найден. " \
-                       "Если произошла ошибка, пожалуйста, свяжитесь с технической поддержкой @azamkhodzhaev_bot"
-                bot.send_message(
-                    chat_id=message.from_user.id,
-                    text=text
-                )
-
-                return
-
-            email, payment_rate, telegram_id, telegram_username,  = fetch
-            if telegram_id:
-                text = "Адрес электронной почты уже используется. " \
-                       "Если произошла ошибка, пожалуйста, свяжитесь с технической поддержкой @azamkhodzhaev_bot"
-
-                bot.send_message(
-                    chat_id=message.from_user.id,
-                    text=text
-                )
-
-                return
-
-            telegram_id = message.from_user.id
-            telegram_username = message.from_user.username
-
-            channel_id = payment_rates.get(payment_rate.lower())
-            if channel_id is None:
-                return
-
-            invite_chat_link = bot.create_chat_invite_link(
-                chat_id=channel_id,
-                member_limit=1,
-                expire_date=datetime.now() + timedelta(days=7)
-            ).invite_link
-
-            text = f"Ваша ссылка на Telegram-канал: {invite_chat_link}"
-            bot.send_message(
-                chat_id=message.from_user.id,
-                text=text
-            )
-
-            if telegram_username:
-                cursor.execute(
-                    f"UPDATE `users` SET `telegram_id` = '{telegram_id}', `telegram_username` = '{telegram_username}' "
-                    f"WHERE LOWER(`email`) = '{email}'"
-                )
-            else:
-                cursor.execute(
-                    f"UPDATE `users` SET `telegram_id` = '{telegram_id}' WHERE LOWER(`email`) = '{email}'"
-                )
-
-            connection.commit()
+            cursor.execute(query)
 
 
-@server.route(f"/{APP_TOKEN}", methods=["POST"])
-def webhook():
-    json_string = request.get_data().decode("utf-8")
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-
-    return "!", 200
-
-
-@server.route('/add', methods=["GET"])
-def add():
-    email = request.args.get("email").lower()
-    payment_rate = request.args.get("payment_rate").lower()
-
-    if email is None or payment_rate is None:
-        return "!", 400
-
-    connection = pymysql.connect(
-        host=MYSQL_HOST,
-        user=MYSQL_USER,
-        password=MYSQL_PASSWORD,
-        database=MYSQL_DATABASE,
-    )
-    with connection:
-        with connection.cursor() as cursor:
-            try:
-                cursor.execute(
-                    f"INSERT INTO `users` (`email`, `payment_rate`) VALUES ('{email}', '{payment_rate}')"
-                )
-            except pymysql.IntegrityError:
-                cursor.execute(
-                    f"UPDATE `users` SET `payment_rate` = '{payment_rate}' WHERE LOWER(`email`) = '{email}'"
-                )
-            finally:
-                connection.commit()
-
-    return "!", 200
-
-
-@server.route('/remove', methods=["GET"])
-def remove():
+@server.route("/add", methods=["GET"])
+def mysql_add():
+    payment_id = request.args.get("payment_id")
+    payment_rate = request.args.get("payment_rate")
     email = request.args.get("email")
-    if email is None:
+    if payment_id is None or payment_rate is None or email is None:
         return "!", 400
 
-    connection = pymysql.connect(
-        host=MYSQL_HOST,
-        user=MYSQL_USER,
-        password=MYSQL_PASSWORD,
-        database=MYSQL_DATABASE,
+    mysql_query(
+        f"INSERT INTO `users` (`payment_id`, `payment_rate`, `email`) "
+        f"VALUES ('{payment_id}', '{payment_rate}', '{email}')"
     )
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(
-                f"SELECT * FROM `users` WHERE LOWER(`email`) = '{email}'"
-            )
-            fetch = cursor.fetchone()
-            if fetch is None:
-                return
-
-            email, payment_rate, telegram_id, telegram_username,  = fetch
-            if telegram_id:
-                channel_id = payment_rates.get(payment_rate.lower())
-                if channel_id is None:
-                    return
-
-                bot.kick_chat_member(
-                    chat_id=channel_id,
-                    user_id=telegram_id
-                )
-                bot.unban_chat_member(
-                    chat_id=channel_id,
-                    user_id=telegram_id
-                )
-
-                text = "Ваш период оплаты курса закончился. " \
-                       "Если произошла ошибка, пожалуйста, свяжитесь с технической поддержкой @azamkhodzhaev_bot"
-                bot.send_message(
-                    chat_id=telegram_id,
-                    text=text
-                )
-
-            cursor.execute(
-                f"DELETE FROM `users` WHERE LOWER(`email`) = '{email}'"
-            )
-            connection.commit()
-
     return "!", 200
 
 
-@server.route('/username_by_email', methods=["GET"])
-def username_by_email():
-    email = request.args.get("email")
-    if email is None:
+def mysql_remove():
+    payment_id = request.args.get("payment_id")
+    if payment_id is None:
         return "!", 400
 
-    connection = pymysql.connect(
-        host=MYSQL_HOST,
-        user=MYSQL_USER,
-        password=MYSQL_PASSWORD,
-        database=MYSQL_DATABASE,
+    mysql_query(
+        f"DELETE FROM `users` WHERE `payment_id` = '{payment_id}'"
     )
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(
-                f"SELECT `telegram_username` FROM `users` WHERE LOWER(`email`) = '{email}'"
-            )
-            fetch = cursor.fetchone()
-
-            telegram_username = fetch[0] if fetch else ""
-
-        return telegram_username, 200
+    return "!", 200
 
 
 if __name__ == '__main__':
